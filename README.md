074645390ee1d0345abc86f007790f6942d40899
Bishwash Gurung <bishwashgurung883@gmail.com>
2024-06-04 06:51:01 +0545

    completing the project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # URL Shortener Microservice
 
-This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+This was the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+
+I added some code to perform the task given in the project.
--- index.js
+++ index.js
@@ -1,24 +1,62 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dns = require("dns");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+// Added Code
+
+let urlCounter = 1;
+const urlMap = new Map();
+
+app.post("/api/shorturl", (req, res) => {
+  const { url: inputUrl } = req.body;
+  const urlPattern = new RegExp(/^(http|https):\/\/[^ "]+$/);
+  if (!urlPattern.test(inputUrl)) {
+    return res.json({ error: "invalid url" });
+  }
+  const hostname = new URL(inputUrl).hostname;
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      return res.json({ error: "invalid url" });
+    }
+  });
+  for (const [shortUrl, originalUrl] of urlMap) {
+    if (originalUrl === inputUrl) {
+      return res.json({ original_url: inputUrl, short_url: shortUrl });
+    }
+  }
+  urlMap.set(urlCounter, inputUrl);
+  res.json({ original_url: inputUrl, short_url: urlCounter });
+  urlCounter++;
+});
+
+app.get("/api/shorturl/:shortUrl", (req, res) => {
+  const shortUrl = parseInt(req.params.shortUrl);
+  if (!urlMap.has(shortUrl)) return res.status(404).json("No URL found");
+  res.redirect(urlMap.get(shortUrl));
+});
+
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
